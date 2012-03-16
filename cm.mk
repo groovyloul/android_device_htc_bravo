cec88c5008f1a31ef398042bf9f569821d200865
Daniel Bergløv <webgeek.eu@gmail.com>
2012-03-16 18:53:56 +0100

    Switch to ICS fingerprint

--- cm.mk
+++ cm.mk
@@ -15,9 +15,9 @@ PRODUCT_BRAND := google
 PRODUCT_DEVICE := bravo
 PRODUCT_MODEL := Desire
 PRODUCT_MANUFACTURER := HTC
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=bravo BUILD_ID=GRK39F \
-BUILD_FINGERPRINT=google/bravo/bravo:2.3.6/GRK39F/189904:user/release-keys \
-PRIVATE_BUILD_DESC="bravo-user 2.3.6 GRK39F 189904 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=bravo BUILD_ID=IML74K \
+BUILD_FINGERPRINT=google/bravo/bravo:4.0.3/IML74K/189904:user/release-keys \
+PRIVATE_BUILD_DESC="bravo-user 4.0.3 IML74K 189904 release-keys"
 
 #
 # Set up the product codename, build version & MOTD.
